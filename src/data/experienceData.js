f95250b0eb8230c406c030918d1b53888a55e29a
Hayat M <hayat28mir@gmail.com>
2023-10-20 16:14:18 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,7 +2,7 @@ const jobs = [
   {
     
     descriptions: [
-      "Have built personal projects like portfolio,todolist,mini games,brand landing page etc web applications using <b>JavaScript</b>, <b>React.js</b>
+      "Have built personal projects like portfolio,todolist,mini games,brand landing page etc web applications using <b>JavaScript</b>, <b>React.js</b>"
     ],
   },
 ];
