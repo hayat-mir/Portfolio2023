ad1ae30b1bcbce7348197f9c0f7225d106a276ed
Hayat M <hayat28mir@gmail.com>
2023-10-20 17:15:22 +0530

    updated projects

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,8 +1,7 @@
 const jobs = [
   {
-    
     descriptions: [
-      "Have built personal projects like portfolio,todolist,mini games,brand landing page etc web applications using <b>JavaScript</b>, <b>React.js</b>"
+      "Have built personal projects like portfolio,todolist,mini games,brand landing page etc web applications using <b>JavaScript</b>, <b>React.js</b>",
     ],
   },
 ];
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,14 +4,7 @@ export const projects = [
     title: "Personal Portfolio",
     description:
       "Platform built with HTML, CSS and javaScript It has cool features like animation, single-page application, responsive design etc. ",
-    tech: [
-      "JavaScript",
-      "ReactJS",
-      "Node.js",
-      "Material-UI",
-      "React Router",
-     
-    ],
+    tech: ["JavaScript", "ReactJS", "Node.js", "Material-UI", "React Router"],
     img: [
       "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBvcnRmb2xpbyUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D",
       "https://res.cloudinary.com/dq6iduc79/image/upload/c_scale,h_1080,w_1920/v1663152715/Portfolio/birthday-1920_ivkx4i.webp",
@@ -19,7 +12,6 @@ export const projects = [
     links: {
       url: "https://hayatportfolio.vercel.app/",
       github: "https://github.com/hayat-mir/portfolio2023",
-      
     },
   },
   {
@@ -33,53 +25,42 @@ export const projects = [
       "https://res.cloudinary.com/dq6iduc79/image/upload/c_scale,h_1080,w_1920/v1663151675/Portfolio/Mobile-App-Screen-Mockups_4_t8lh7i.webp",
     ],
     links: {
-      url: "todolist-hayat528.vercel.app",
+      url: "https://todolist-hayat528.vercel.app/",
       github: "https://github.com/hayat-mir/todolist",
-     
     },
   },
   {
     id: 3,
     title: "brand landing page",
-    description:
-      "Developed with HTML,CSS and JavaScript",
-    tech: [
-      "JavaScript",
-      "css",
-      "html"
-      "Bootstrap",
-      
-    ],
+    description: "Developed with HTML,CSS and JavaScript",
+    tech: ["JavaScript", "css", "html", "Bootstrap"],
     img: [
       "https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YnJhbmQlMjBsYW5kaW5nJTIwcGFnZXxlbnwwfHwwfHx8MA%3D%3D",
       "https://res.cloudinary.com/dq6iduc79/image/upload/c_scale,h_1080,w_1920/v1663151813/Portfolio/url-shortener_mhrgh3.webp",
     ],
     links: {
+      url: "https://brand-landing-page-alpha.vercel.app/",
       github: "https://github.com/hayat-mir/Brand-landing-page",
-    
     },
   },
   {
     id: 4,
     title: "Modern landing Page",
-    description:
-      "MOdern landing Page",
+    description: "MOdern landing Page",
     tech: [
       "JavaScript",
       "ReactJS",
       "React Router",
       "Material-UI",
       "Framer Motion",
-      
     ],
     img: [
       "https://res.cloudinary.com/dq6iduc79/image/upload/c_scale,h_1080,w_1920/v1663151598/Portfolio/18334773_171-min_o0vtqc.webp",
       "https://res.cloudinary.com/dq6iduc79/image/upload/c_scale,h_1080,w_1920/v1663151582/Portfolio/bohetnbuoauuu_pfiaix.webp",
     ],
     links: {
-      url: "https://todolist-hayat528.vercel.app/",
+      url: "https://hayatportfolio.vercel.app/",
       github: "https://github.com/hayat-mir",
-      
     },
   },
 ];
@@ -88,8 +69,7 @@ export const openSourceProjects = [
   {
     id: 1,
     title: "Hayat",
-    description:
-      "Hayat",
+    description: "Hayat",
     tech: [
       "JavaScript",
       "ReactJS",
@@ -100,11 +80,10 @@ export const openSourceProjects = [
       "CSS",
     ],
     img: [
-     
       "https://res.cloudinary.com/dq6iduc79/image/upload/v1662896618/portfolio-avif/FireShot_Capture_070_-_Milan_-_Home_-_milaan.vercel.app_nv20hg.webp",
     ],
     links: {
-      url: "https://todolist-hayat528.vercel.app/",
+      url: "https://hayatportfolio.vercel.app/",
       github: "https://github.com/hayat-mir",
     },
   },
