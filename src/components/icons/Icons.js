b15e0bb55c72e37bd757482f78c9dac5c11a0932
Hayat M <hayat28mir@gmail.com>
2023-10-21 20:31:20 +0530

    added buy me a coffee icon

--- src/components/icons/Icons.js
+++ src/components/icons/Icons.js
@@ -1,8 +1,12 @@
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import CoffeeIcon from "@mui/icons-material/Coffee";
 import clsx from "clsx";
 import { useStyles } from "./Icons-style";
 
+const iconStyle = {
+  fill: "hsl(50, 100%, 70%)",
+};
 const Icons = (props) => {
   const classes = useStyles();
   return (
@@ -20,6 +24,13 @@ const Icons = (props) => {
         rel="noopener noreferrer"
       >
         <LinkedInIcon fontSize="large" />
+      </a>
+      <a
+        href="https://www.buymeacoffee.com/hayatm/e/176901"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <CoffeeIcon style={iconStyle} fontSize="large" />
       </a>
     </div>
   );
