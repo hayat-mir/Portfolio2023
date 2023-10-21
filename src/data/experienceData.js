f5846e54ff8bc16dcf77aaf40ee37044efdace4f
Hayat M <hayat28mir@gmail.com>
2023-10-21 12:36:53 +0530

    updated experience section

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,7 +1,7 @@
 const jobs = [
   {
     descriptions: [
-      "Have built personal projects like portfolio,todolist,mini games,brand landing page etc web applications using <b>JavaScript</b>, <b>React.js</b>",
+      "Have built personal projects like <b>Personal portfolio,Weather App,todolist,mini games,Modern landing page</b> etc web applications using <b>Html</b>,<b>CSS</b>,<b>JavaScript</b>,<b>React</b>,<b>Tailwind CSS</b> ,<b>React.js</b>",
     ],
   },
 ];
