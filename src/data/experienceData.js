ee7962135bb32c5ab5923aaef8b45d45f9e4ab5d
Hayat M <hayat28mir@gmail.com>
2023-12-12 09:12:45 +0530

    updated experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,7 +3,8 @@ const jobs = [
     title: "WEB DEVELOPMENT",
     company: "PERSONAL PROJECTS",
     descriptions: [
-      "Developed a full stack Uber Ride application with <b>Next.js 13</b>, <b>tailwindcss</b> ,<b>google api</b> and <b>stripe </b> and other technologies. ",
+      "working as technical writer with <b>Cyber Spark </b>  ",
+      "<br>Since December 2023",
       " Proficient in creating dynamic and responsive web applications.",
       "Well-versed in popular frameworks such as React and Next.js.",
       ,
